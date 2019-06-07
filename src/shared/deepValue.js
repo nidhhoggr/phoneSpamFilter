9478da3cbab1ad6bbba72f83004d07d9e70fbb63
nidhhoggr <persie.joseph@gmail.com>
2019-06-06 21:59:40 -0400

    return deepvalues of 0

--- src/shared/deepValue.js
+++ src/shared/deepValue.js
@@ -1,4 +1,4 @@
 module.exports = (app) => 
   (object, path, defaultValue) => 
     path.split('.').reduce((xs, x) => 
-      (xs && xs[x]) ? xs[x] : defaultValue, object)
+      (xs && typeof xs[x] !== "undefined" && xs[x] !== null) ? xs[x] : defaultValue, object)
